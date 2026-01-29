33755c560eb68b691b0fdd5e9a06eb0f44949948
hemang82 <109712867+hemang82@users.noreply.github.com>
2026-01-29 16:07:11 +0530

    update code

--- src/components/user-profile/UserInfoCard.tsx
+++ src/components/user-profile/UserInfoCard.tsx
@@ -19,6 +19,7 @@ export default function UserInfoCard({ user }: UserCardProps) {
     <div className="p-5 border border-gray-200 rounded-2xl dark:border-gray-800 lg:p-6">
       <div className="flex flex-col gap-6 lg:flex-row lg:items-start lg:justify-between">
         <div>
+          
           <h4 className="text-lg font-semibold text-gray-800 dark:text-white/90 lg:mb-6">
             Personal Information
           </h4>
