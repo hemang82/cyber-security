f46a486f0d9ab131b7afd0a173925c594c05c9d6
hemang82 <109712867+hemang82@users.noreply.github.com>
2026-03-02 17:49:58 +0530

    update code

--- next.config.ts
+++ next.config.ts
@@ -2,25 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
     return config;
-  },
-
-  turbopack: {
-    rules: {
-      '*.svg': {
-        loaders: ['@svgr/webpack'],
-        as: '*.js',
-      },
-    },
   },
 };
 
